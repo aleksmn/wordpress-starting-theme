bd3defdd70359fc51e31c3eaa1692c4f0ab42b1d
Mike <al-mn@ya.ru>
2018-12-14 21:42:27 +0300

    initial

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+<footer class="container">
+&copy; Михаил Алексадров, 2018
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div class="container pt-5 pb-5">
+
+  <h1 class="pb-3"><?php the_title(); ?></h1>
+
+  <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
+
+    <?php the_content(); ?>
+
+  <?php endwhile; endif; ?>
+
+
+  <!--   
+    <div class="row"> 
+      <div class="col">Left side</div>
+      <div class="col">Right side</div>
+    </div> -->
+
+</div>
+
+<?php  get_footer(); ?>
